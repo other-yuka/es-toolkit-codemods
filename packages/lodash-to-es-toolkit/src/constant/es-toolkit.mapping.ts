053d0e2d1aa90c07c8502336658d2d32f017fbae
other-yuka <43230730+other-yuka@users.noreply.github.com>
2025-07-03 00:15:18 +0000

    chore: update es-toolkit mapping data for 1.39.6

--- packages/lodash-to-es-toolkit/src/constant/es-toolkit.mapping.ts
+++ packages/lodash-to-es-toolkit/src/constant/es-toolkit.mapping.ts
@@ -1,5 +1,5 @@
 // This file is auto-generated. Do not edit manually.
-// Generated from es-toolkit version 1.39.5
+// Generated from es-toolkit version 1.39.6
 
 import type { CompatibilityMapping } from '../types';
 
